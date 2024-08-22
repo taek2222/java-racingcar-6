45826c8f3e1d0daeef3175199f23b12d8ec6e75a
taek2222 <oot309@daum.net>
2024-08-22 20:03:28 +0900

    feat: 입출력 기능 추가 | @taek2222

---
+++ src/main/java/racingcar/view/Output.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+public abstract class Output {
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+}
