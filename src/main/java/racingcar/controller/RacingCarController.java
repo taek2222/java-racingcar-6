5c364413e70113db4f4cd96fec8b8d6529675991
taek2222 <oot309@daum.net>
2024-08-24 18:22:47 +0900

    refactor: Service 비즈니스 로직 Controller 이동 및 init 메서드 분할 | @hjlim456

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,17 +1,64 @@
 package racingcar.controller;
 
 import racingcar.domain.car.*;
-import racingcar.service.car.*;
 import racingcar.validate.CarValidator;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+import static racingcar.view.car.CarOutput.*;
+import static racingcar.view.car.CarOutput.println;
 
 public class RacingCarController {
     private final CarSetting carSetting = new CarSetting();
     private final CarValidator carValidator = new CarValidator();
     private final CarMove carMove = new CarMove();
     private final CarWinner carWinner = new CarWinner();
-    private final CarService carService = new CarService(carSetting, carValidator, carMove, carWinner);
 
     public void run() {
-        carService.processCarPlay();
+        List<Car> carList = initCar();
+        int playCount = initPlayCount();
+
+        printResultStartMessage();
+        for (int playing = 0; playing < playCount; playing++) {
+            carMoves(carList);
+            racingResult(carList);
+        }
+
+        int winnerDistance = carWinner.getWinnerDistance(carList);
+        ArrayDeque<String> winnerList = carWinner.getWinnerList(carList, winnerDistance);
+        printWinnerList(winnerList);
+    }
+
+    private List<Car> initCar() {
+        printCarPlayCountInputMessage();
+        String[] cars = carSetting.inputCarName();
+        carValidator.carNameValidator(cars);
+
+        List<Car> carList = new ArrayList<>();
+        for (String car : cars)
+            carList.add(carSetting.saveCar(car));
+
+        return carList;
+    }
+
+    private int initPlayCount() {
+        printCarNameInputMessage();
+        return carSetting.inputCarPlayCount();
+    }
+
+    private void carMoves(List<Car> carList) {
+        for (Car car : carList)
+            carMove.moveCar(car);
+    }
+
+    private void racingResult(List<Car> carList) {
+        for (Car car : carList) {
+            printResultCarNameMessage(car.getName());
+            printResultCarDistanceMessage(car.getDistance());
+            println();
+        }
+        println();
     }
 }
